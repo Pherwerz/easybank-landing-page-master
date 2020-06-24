82128658e3e6c89109d2886acffe702d6d37a377
pherwerz <lordrisk101@gmail.com>
2020-06-24 22:55:07 +0100

    finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './main.scss';
+
+const navButton = document.querySelector('.header__btn');
+const nav = document.querySelector('.header__navigation');
+const img = document.querySelector('#img');
+let tracker = true;
+
+navButton.addEventListener('click', () => {
+  if (tracker) {
+    img.src = './img/icon-close.svg';
+    nav.style.visibility = 'visible';
+    nav.style.opacity = '1';
+    tracker = false;
+  } else {
+    img.src = './img/icon-hamburger.svg';
+    nav.style.visibility = 'hidden';
+    nav.style.opacity = '0';
+    tracker = true;
+  }
+});
